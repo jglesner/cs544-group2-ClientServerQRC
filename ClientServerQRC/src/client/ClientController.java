858342d606dc9541c937b08d400df6643f7ba4c9
Jeremy Glesner <jeremy@bericotechnologies.com>
2012-05-05 17:04:38 -0400

    Wrote code to establish communication between client and server.

--- ClientServerQRC/src/client/ClientController.java
+++ ClientServerQRC/src/client/ClientController.java
@@ -14,7 +14,11 @@ public class ClientController {
 
 	private boolean connected; //status for listening		
 	private Socket serverSocketConn;
-	
+	/** For reading input from socket */
+    private BufferedReader br;
+
+    /** For writing output to socket. */
+    private PrintWriter pw;	
 	
 	public void start()
 	{
@@ -42,7 +46,13 @@ public class ClientController {
 			{
 			  InetAddress server = InetAddress.getLocalHost();
 			  serverSocketConn = new Socket(server , 5555);
-			  // send and receive data...
+	          openChannel(serverSocketConn);
+	          
+			  System.out.println(br.readLine());
+			  pw.print("test");
+	          System.out.println(br.readLine());
+
+	          
 			}
 			catch (UnknownHostException ex) {
 			  System.err.println(ex);
@@ -53,6 +63,19 @@ public class ClientController {
 			connected = true;
 	    }
 	}	
+	
+    public void openChannel(Socket socket) throws IOException {
+        this.serverSocketConn = socket;
+        //get I/O from socket
+        try {
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(socket.getOutputStream(), true);
+        }
+        catch (IOException ioe) {
+            throw ioe;
+        }
+    }	
+	
 	
 	public void closeToServer() {
 		if (connected) {	
